e99d5b39e8e1b07697e1c22c668e14f77e8342c3
Pierre Nogaro <pierre.nogaro@gmail.com>
2023-10-20 14:11:54 +0200

    Sauvegarde calculatrice

---
+++ main.js
@@ -0,0 +1,15 @@
+const zero = document.querySelector('.zero')
+
+const un = document.querySelector('.un')
+const deux = document.querySelector('.deux')
+const trois = document.querySelector('.trois')
+
+const quatre = document.querySelector('.quatre')
+const cinq = document.querySelector('.cinq')
+const six = document.querySelector('.six')
+
+const sept = document.querySelector('.sept')
+const huit = document.querySelector('.huit')
+const neuf = document.querySelector('.neuf')
+
+
