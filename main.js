f67166d38515454b065860aa48d537158f046ffa
Pierre Nogaro <pierre.nogaro@gmail.com>
2023-10-30 08:24:29 +0100

    fin

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-const zero = document.querySelector('.zero')
+// DOM
+const touches = [...document.querySelectorAll('.bouton')];
+const listeKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
 
-const un = document.querySelector('.un')
-const deux = document.querySelector('.deux')
-const trois = document.querySelector('.trois')
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
 
-const quatre = document.querySelector('.quatre')
-const cinq = document.querySelector('.cinq')
-const six = document.querySelector('.six')
+})
 
-const sept = document.querySelector('.sept')
-const huit = document.querySelector('.huit')
-const neuf = document.querySelector('.neuf')
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
 
+})
 
+const calculer = (valeur) => {
+    if (listeKeycode.includes(valeur)) {
+        switch (valeur) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML;
+        }
+    }
+}
+
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue dans votre calcul : ' + e.message)
+})
